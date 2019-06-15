63c847d1a4fccc4de04ddc5130dc46b4ed1ffde4
Liam Hammett <liamhammett@gmail.com>
2019-06-15 18:56:02 +0100

    Add back PurgeCSS exceptions

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,22 +15,8 @@ module.exports = {
                 'Segoe UI Symbol',
                 'Noto Color Emoji',
             ],
-            mono: [
-                'IBM Plex Mono',
-                'Monaco',
-                'Consolas',
-                'Liberation Mono',
-                'Courier New',
-                'monospace',
-            ],
-            serif: [
-                'Merriweather',
-                'Georgia',
-                'Cambria',
-                '"Times New Roman"',
-                'Times',
-                'serif',
-            ],
+            mono: ['IBM Plex Mono', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
+            serif: ['Merriweather', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
             slab: [
                 'Zilla Slab',
 
@@ -47,7 +33,7 @@ module.exports = {
                 'Segoe UI Emoji',
                 'Segoe UI Symbol',
                 'Noto Color Emoji',
-            ]
+            ],
         },
         extend: {
             borderWidth: {
@@ -55,11 +41,11 @@ module.exports = {
                 5: '5px',
             },
             colors: {
-                'twitter': '#1da1f2',
+                twitter: '#1da1f2',
                 'stack-overflow': '#f48024',
-                'dribbble': '#ea4c89',
-                'github': '#4078c0',
-                'medium': '#00ab6c',
+                dribbble: '#ea4c89',
+                github: '#4078c0',
+                medium: '#00ab6c',
             },
             fontSize: {
                 xxs: '0.65rem',
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
-const mix = require('laravel-mix')
+const mix = require('laravel-mix');
 
-require('laravel-mix-tailwind')
-require('laravel-mix-purgecss')
+require('laravel-mix-tailwind');
+require('laravel-mix-purgecss');
 
 mix.js('resources/js/app.js', 'public/js')
 
@@ -23,9 +23,17 @@ mix.js('resources/js/app.js', 'public/js')
         },
     })
 
-    .sourceMaps()
+    .sourceMaps();
 
 if (mix.inProduction()) {
-    mix.purgeCss()
-        .version()
+    mix.purgeCss({
+            globs: [
+                path.join(__dirname, 'node_modules/simplemde/**/*.js'),
+                path.join(__dirname, 'node_modules/turbolinks/**/*.js'),
+                path.join(__dirname, 'vendor/spatie/menu/**/*.php'),
+                path.join(__dirname, 'vendor/scrivo/highlight.php/**/*.php'),
+            ],
+            whitelistPatterns: [/carbon/, /language/, /hljs/, /cm-/, /alert-/, /page/, /iframe/],
+        })
+        .version();
 }
